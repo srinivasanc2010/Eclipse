223f0854f19c6dde9bb0b7d0199d0c6794dc7e53
sriniintraining <sriniintraining@sriniinwebtraining-inspiron-7737.Dlink>
2019-11-30 01:50:42 +0530

    updated

--- teluskoJava/src/variableDemo.java
+++ teluskoJava/src/variableDemo.java
@@ -9,7 +9,7 @@ public class variableDemo {
 		//byte b=5;       // 1 byte  -> 8 bits  -> -128 to 127
 		// l=5000000000000000000l; 	    // 8 bytes -> 64 bits
 		
-		int a=(int)5.8;   //type casting // explicit conversion
+		int a=(int)5.9;   //type casting // explicit conversion
 		System.out.println(a);
 		
 		double d=5;      // implicit conversion
